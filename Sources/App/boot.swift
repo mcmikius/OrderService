ecb687ba49cc7a64c3ea7065579ea1e1ae84b534
Mykhailo Bondarenko <mcmikius@icloud.com>
2020-03-17 16:32:17 +0200

    add boot

---
+++ Sources/App/boot.swift
@@ -0,0 +1,43 @@
+//
+//  File.swift
+//  
+//
+//  Created by Mykhailo Bondarenko on 17.03.2020.
+//
+
+import Vapor
+import NIO
+
+private var verifyOrdersTask: RepeatedTask? = nil
+
+public func boot(_ app: Application) throws {
+    
+    let url = Environment.get("PRODUCT_SERVICE_URL") ?? "http://localhost:8080/v1/products?ids="
+
+    let processingController = ProcessingController(url)
+    
+    verifyOrdersTask = app.eventLoopGroup.next().scheduleRepeatedAsyncTask(
+        initialDelay: .seconds(0),
+        delay: .seconds(60)
+    ) { [weak app] task -> EventLoopFuture<Void> in
+        let app = app!
+        
+        var returnFuture:EventLoopFuture<Void>
+        
+        do {
+            returnFuture = try processingController.getOrders(status: 0, on: app).flatMap { orders -> EventLoopFuture<Void> in
+                var processingFutures:[EventLoopFuture<Void>] = []
+                
+                for order in orders {
+                    processingFutures.append(processingController.processOrderInformation(order, on: app).transform(to: ()))
+                }
+                return processingFutures.flatten(on: app.eventLoopGroup.next())
+            }
+        }
+        catch let error {
+            returnFuture = app.eventLoopGroup.next().makeFailedFuture(error)
+        }
+        
+        return returnFuture
+    }
+}
